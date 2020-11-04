4c7c088ce6e2574cf98115fea02036a90bd73a43
Sarah Verkade <sarahverkade@live.nl>
2020-11-04 12:37:48 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,22 @@
+let hamburger = document.querySelector("button img");
 
+let showMenu = document.querySelector("header nav ul:last-of-type");
+
+let buttonClose = document.querySelector("button");
+
+let hideScroll = document.querySelector('body');
+
+
+hamburger.addEventListener("click", function () {
+    showMenu.classList.toggle("showMenu");
+    buttonClose.classList.toggle("button-close");
+    hideScroll.classList.toggle('hide-scroll');
+}, false);
+
+hamburger.addEventListener("click", function myFunction() {
+    if (hamburger.style.display === "none") {
+        hamburger.style.display = "block";
+    } else {
+        hamburger.style.display = "none";
+    }
+}, false);
